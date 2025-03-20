26e3dd8476041a55f99dac30b0f0e305bd854e38
01Ani <95162490+01Ani@users.noreply.github.com>
2025-03-20 14:37:25 +0530

    Added options for making new and editing product
    We established CRUD. Without still implementing the delete phase.

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const Product = require("./models/product");
-const app = express();
+const methodOverride = require("method-override");
 
+const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/sg-ecomwebsite")
@@ -30,9 +33,33 @@ app.get("/products", async (req, res) => {
   res.render("products/index", { products });
 });
 
+app.post("/products", async (req, res) => {
+  const product = new Product(req.body.product);
+  await product.save();
+  res.redirect(`/products/${product._id}`);
+  // res.send(req.body);
+});
+
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
 app.get("/products/:id", async (req, res) => {
   const product = await Product.findById(req.params.id);
   res.render("products/show", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, {
+    ...req.body.product,
+  });
+  res.redirect(`/products/${product._id}`);
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.render("products/edit", { product });
 });
 
 app.get("/", (req, res) => {
