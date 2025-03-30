e4eb63b6fc648b96d3a4dbffc2ede92d336578a5
01Ani <95162490+01Ani@users.noreply.github.com>
2025-03-30 21:27:43 +0530

    Added and implemented expressError and catchAsync
    Async errors are now handled and messages are also thrown back.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const Product = require("./models/product");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
+const expressError = require("./utils/expressError");
 
 const app = express();
 app.engine("ejs", ejsMate);
@@ -36,47 +38,75 @@ mongoose
 //   res.send(prod);
 // });
 
-app.get("/products", async (req, res) => {
-  const product = await Product.find({});
-  res.render("products/index", { product });
-});
+app.get(
+  "/products",
+  catchAsync(async (req, res) => {
+    const product = await Product.find({});
+    res.render("products/index", { product });
+  })
+);
 
-app.post("/products", async (req, res) => {
-  const product = new Product(req.body.product);
-  await product.save();
-  res.redirect(`/products/${product._id}`);
-  // res.send(req.body);
-});
+app.post(
+  "/products",
+  catchAsync(async (req, res) => {
+    if (!req.body.product) throw new expressError("Invalid Product", 400);
+    const product = new Product(req.body.product);
+    await product.save();
+    res.redirect(`/products/${product._id}`);
+    // res.send(req.body);
+  })
+);
 
 app.get("/products/new", (req, res) => {
   res.render("products/new");
 });
 
-app.get("/products/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.render("products/show", { product });
-});
+app.get(
+  "/products/:id",
+  catchAsync(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render("products/show", { product });
+  })
+);
 
-app.put("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, {
-    ...req.body.product,
-  });
-  res.redirect(`/products/${product._id}`);
-});
+app.put(
+  "/products/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, {
+      ...req.body.product,
+    });
+    res.redirect(`/products/${product._id}`);
+  })
+);
 
-app.delete("/products/:id", async (req, res) => {
-  await Product.findByIdAndDelete(req.params.id);
-  res.redirect("/products");
-});
+app.delete(
+  "/products/:id",
+  catchAsync(async (req, res) => {
+    await Product.findByIdAndDelete(req.params.id);
+    res.redirect("/products");
+  })
+);
 
-app.get("/products/:id/edit", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.render("products/edit", { product });
-});
+app.get(
+  "/products/:id/edit",
+  catchAsync(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render("products/edit", { product });
+  })
+);
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.all("*", (req, res, next) => {
+  next(new expressError("Page not found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
